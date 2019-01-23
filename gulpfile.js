f04fdddff993a418c346ce018d85de1d94f32406
segur <segur.opus@gmail.com>
2019-01-23 19:17:51 +0900

    Add watching system

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
 
-gulp.task("sass", function () {
-  gulp.src("sass/**/*.scss")
+gulp.task("default", function () {
+  gulp.watch("sass/**/*.scss", function(){
+    gulp.src("sass/**/*.scss")
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(gulp.dest("./css"))
+  });
 })
