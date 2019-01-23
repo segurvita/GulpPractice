5063c87de8df6a2036bf60f2c89a7b273a8b96c9
segur <segur.opus@gmail.com>
2019-01-23 19:21:12 +0900

    Add comment

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
+// gulpプラグインの読み込み
 var gulp = require("gulp");
+// Sassをコンパイルするプラグインの読み込み
 var sass = require("gulp-sass");
 
+// style.scssの監視タスクを作成する
 gulp.task("default", function () {
-  gulp.watch("sass/**/*.scss", function(){
+  // scssファイルを監視
+  gulp.watch("sass/**/*.scss", function () {
+    // scssファイルを取得
     gulp.src("sass/**/*.scss")
-    .pipe(sass({ outputStyle: 'expanded' }))
-    .pipe(gulp.dest("./css"))
+      // Sassのコンパイルを実行
+      .pipe(sass({
+        outputStyle: 'expanded'
+      }))
+      // cssフォルダーに保存
+      .pipe(gulp.dest("./css"))
   });
 })
