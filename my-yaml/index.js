b598f00c93aee8c2a7cc8c275b42cac665938efa
segur <segur.opus@gmail.com>
2019-01-25 12:09:38 +0900

    Add function to remove swagger

--- my-yaml/index.js
+++ my-yaml/index.js
@@ -1,32 +1,47 @@
-var gutil = require('gulp-util');
-var through = require('through2');
-var path = require('path');
+var gutil = require("gulp-util");
+var through = require("through2");
+var path = require("path");
+var swaggerRmExample = require("swagger-rm-example");
 
-module.exports = () => through.obj((file, enc, callback) => {
-  // 中身がないとき
-  if (file.isNull()) {
-    callback(null, file);
-    return;
-  }
-
-  // 中身がstreamのとき
-  if (file.isStream()) {
-      callback(new gutil.PluginError('my-yaml', 'Streams not supported'));
+module.exports = () =>
+  through.obj((file, enc, callback) => {
+    // 中身がないとき
+    if (file.isNull()) {
+      callback(null, file);
       return;
-  }
+    }
 
-  // 中身がBufferのとき
-  if (file.isBuffer()) {
+    // 中身がstreamのとき
+    if (file.isStream()) {
+      callback(new gutil.PluginError("my-yaml", "Streams not supported"));
+      return;
+    }
+
+    // 中身がBufferのとき
+    if (file.isBuffer()) {
       // Bufferを文字列に変換
-      const text = file.contents.toString('utf8');
-      callback(null, new gutil.File({
+      const strInput = file.contents.toString("utf8");
+
+      // exampleがあると、API Gateway での取り込み時に失敗するので削除する
+      const strRmExample = swaggerRmExample.removeExample(strInput);
+      if (!strRmExample) {
+        callback(
+          new gutil.PluginError("my-yaml", "removing example is failed.")
+        );
+        return;
+      }
+
+      callback(
+        null,
+        new gutil.File({
           base: file.base,
           cwd: file.cwd,
-          path: gutil.replaceExtension(file.path, '.yml'),
-          contents: new Buffer(text, 'utf8')
-      }));
+          path: gutil.replaceExtension(file.path, ".yml"),
+          contents: new Buffer(strRmExample, "utf8")
+        })
+      );
       return;
-  }
-  // 何もしない
-  callback(null, file);
-});
+    }
+    // 何もしない
+    callback(null, file);
+  });
